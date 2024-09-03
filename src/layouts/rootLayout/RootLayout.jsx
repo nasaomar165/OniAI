f8cf1a5b0068ecc61d91aa70e7b116ec17e2b492
Omar Hisham <omar.hisham0597@gmail.com>
2024-09-03 16:57:48 +0300

    Update RootLayout.jsx

--- src/layouts/rootLayout/RootLayout.jsx
+++ src/layouts/rootLayout/RootLayout.jsx
@@ -2,7 +2,8 @@ import { Link, Outlet } from "react-router-dom";
 import "./rootLayout.css";
 import { ClerkProvider, SignedIn, UserButton } from "@clerk/clerk-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key");
 }
